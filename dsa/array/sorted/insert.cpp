6046eaf3dfeed5d9721effb9b506a9dc59ddf65a
Karim Elkhanoufi <126080269+karimelkh@users.noreply.github.com>
2023-10-12 21:22:22 +0100

    update sorted array
    document code

--- dsa/array/sorted/insert.cpp
+++ dsa/array/sorted/insert.cpp
@@ -1,34 +1,47 @@
 #include <iostream>
-#include "arr.h"
+#include "../common/arr.h" // import arr.h that includes print_arr and type_of_sort functions
 
 using namespace std;
 
 void print_arr(int, int, int, int);
+
 int type_of_sort(int, int);
 
-int insert_asc(int arr[], int capacity, int n, int value) {
-  if(n + 1 >= capacity) return n;
+/*
+  * insert the value in ascendent array
+  * returns the new size if the inserting was successful
+  * else the returns old size
+*/
+int insert_asc(int arr[], int capacity, int size, int value) {
+  if(size >= capacity) return size;
   int pos = 0;
-  while(pos <= n) {
+  
+  // loop until find the appropriate position
+  while(pos < size) {
     if(arr[pos] >= value) break;
     pos++ ;
   }
-  for(int i = n; i >= pos; i--)
+  // shift all the elements after pos to the right
+  for(int i = size - 1; i >= pos; i--)
     arr[i + 1] = arr[i];
   arr[pos] = value;
-  return n + 1;
+  return size + 1;
 }
 
 int main() {
-  int arr[10] = {0, 1, 2, 4, 5, 6, 7, 8, 9};
-  int n = 8;
+  int arr[10] = {1, 2, 4, 5, 6, 7, 8, 9};
+  int size = 8;
+
   cout << "before           : ";
-  print_arr(arr, 10);
-  n = insert_asc(arr, 10, n, 3);
+  print_arr(arr, 10, 0, size - 1); // output: 1 2 4 5 6 7 8 9
+
+  size = insert_asc(arr, 10, size, 3); // insert value 3
   cout << "after inserting 3: ";
-  print_arr(arr, 10);
-  n = insert_asc(arr, 10, n, 4);
+  print_arr(arr, 10, 0, size - 1); // output: 1 2 3 4 5 6 7 8 9
+
+  size = insert_asc(arr, 10, size, 4); // insert value 4
   cout << "after inserting 4: ";
-  print_arr(arr, 10);
+  print_arr(arr, 10, 0, size - 1); // output:  1 2 3 4 4 5 6 7 8 9
+
   return 0;
 }
--- dsa/array/sorted/search.cpp
+++ dsa/array/sorted/search.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 using namespace std;
 
+/*
+  * search for the target in the array using the binary search algorithm
+  * returns the index of the target if exist else returns -1
+*/
 int search_bin(int arr[], int index_of_last, int target) {
   int beg = 0, end = index_of_last, mid;
   while(beg <= end) {
-    mid = (beg + end) / 2;
-    if(arr[mid] > target) end = mid - 1;
-    else if(arr[mid] < target) beg = mid + 1;
-    else return mid;
+    mid = (beg + end) / 2; // Calculate the middle index of the current search range.
+    if(arr[mid] > target) end = mid - 1; // adjust `end` to search the lower half of the current range.
+    else if(arr[mid] < target) beg = mid + 1; // adjust `beg` to search the upper half of the current range.
+    else return mid; // target found at index `mid`.
   }
-  return -1;
+  return -1; // target not found
 }
 
 int main() {
   int arr[10] = {10, 11, 12, 15, 17, 19, 30, 31, 41, 45};
 
-  int index = search_bin(arr, 10, 55);
-  if(index != -1)
-    cout << "the index of " << arr[index] << " is: " << index << endl;
-  else
-    cout << "Target not found." << endl;
+  int index = search_bin(arr, 10, 55); // search for 55
+  index == -1 ? cout << "Target not found." << endl : cout << "the index of " << arr[index] << " is: " << index << endl;
+  // output: Target not found.
 
-  index = search_bin(arr, 10, 19);
-  if(index != -1)
-    cout << "the index of " << arr[index] << " is: " << index << endl;
-  else
-    cout << "Target not found." << endl;
+  index = search_bin(arr, 10, 19); // search for 19
+  index == -1 ? cout << "Target not found." << endl : cout << "the index of " << arr[index] << " is: " << index << endl;
+  // output: the index of 19 is: 5
+
   return 0;
 }
