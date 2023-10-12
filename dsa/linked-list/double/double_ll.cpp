37129a03c33b32db19fe0cc70429d547b6f8058b
Karim Elkhanoufi <126080269+karimelkh@users.noreply.github.com>
2023-10-12 14:13:40 +0100

    update double_ll.cpp
    finish code

--- dsa/linked-list/double/double_ll.cpp
+++ dsa/linked-list/double/double_ll.cpp
@@ -25,13 +25,16 @@ public:
   }
 
   void print_list() {
-    node* current_node = head;
-    cout << "head -> ";
-    while(current_node->next != NULL) {
-      cout << current_node->data << " <=> ";
-      current_node = current_node->next;
+    if(is_empty()) cout << EMPTY_LIST_ERROR << endl;
+    else {
+      node* current_node = head;
+      cout << "head -> ";
+      while(current_node->next != NULL) {
+        cout << current_node->data << " <=> ";
+        current_node = current_node->next;
+      }
+      cout << current_node->data << " -> NULL" << endl;
     }
-    cout << current_node->data << " -> NULL" << endl;
   }
 
   node* create_node(int d) {
@@ -39,6 +42,11 @@ public:
   }
 
   void insert_beg(node* new_node) {
+    if(is_empty()) {
+      head = new_node;
+      size++ ;
+      return;
+    }
     if(head->next != NULL) head->next->prev = new_node;
     new_node->next = head;
     head = new_node;
@@ -46,29 +54,35 @@ public:
   }
 
   void insert_end(node* new_node) {
-    if(is_empty()) return;
-    node* last_node = head;
-    while(last_node->next != NULL)
-      last_node = last_node->next;
-    new_node->prev = last_node;
-    last_node->next = new_node;
-    size++ ;
+    if(is_empty()) insert_beg(new_node);
+    else {
+      node* last_node = head;
+      while(last_node->next != NULL)
+        last_node = last_node->next;
+      new_node->prev = last_node;
+      last_node->next = new_node;
+      size++ ;
+    }
   }
 
-  void inser_at(node* new_node, int pos) {
-    if(is_empty()) return;
-    node* node_at_pos = head;
-    int i = 1;
-    while(i < pos) {
-      node_at_pos = node_at_pos->next;
-      i++ ;
+  void insert_at(node* new_node, int pos) {
+    if(pos == 1) insert_beg(new_node);
+    else if(pos == size + 1) insert_end(new_node);
+    else if(pos > size + 1 || pos < 1) cout << INVALID_POS_ERROR << endl;
+    else {
+      node* node_at_pos = head;
+      int i = 1;
+      while(i < pos) {
+        node_at_pos = node_at_pos->next;
+        i++ ;
+      }
+      new_node->next = node_at_pos;
+      new_node->prev = node_at_pos->prev;
+      if(node_at_pos->prev != NULL) node_at_pos->prev->next = new_node;
+      else head = new_node;
+      node_at_pos->prev = new_node;
+      size++ ;
     }
-    new_node->next = node_at_pos;
-    new_node->prev = node_at_pos->prev;
-    if(node_at_pos->prev != NULL) node_at_pos->prev->next = new_node;
-    else head = new_node;
-    node_at_pos->prev = new_node;
-    size++ ;
   }
 
   void delete_beg() {
@@ -87,20 +101,71 @@ public:
       last_node = last_node->next;
     if(last_node->prev != NULL) last_node->prev->next = NULL;
     else head = NULL;
+    size-- ;
   }
 
-  void delete_at() {
-    if(is_empty()) return;
+  void delete_at(int pos) {
+    if(is_empty()) cout << EMPTY_LIST_ERROR << endl;
+    else if(pos > size || pos < 1) cout << INVALID_POS_ERROR << endl;
+    else if(pos == 1) delete_beg();
+    else if(pos == size) delete_end();
+    else {
+      node* node_at_pos = head;
+      int i = 1;
+      while(i < pos) {
+        node_at_pos = node_at_pos->next;
+        i++ ;
+      }
+      node_at_pos->prev->next = node_at_pos->next;
+      node_at_pos->next->prev = node_at_pos->prev;
+      delete node_at_pos;
+      size-- ;
+    }
   }
 
-
+  void reverse_list() {
+    if(is_empty()) cout << EMPTY_LIST_ERROR << endl;
+    else if(size == 1) return;
+    else {
+      node* current_node = head;
+      node* next_node;
+      while(current_node != NULL) {
+        next_node = current_node->next;
+        current_node->next = current_node->prev;
+        current_node->prev = next_node;
+        if(current_node->prev == NULL) head = current_node;
+        current_node = current_node->prev;
+      }
+    }
+  }
 };
 
 int main() {
-  double_list list;
-  list.insert_beg(list.create_node(2));
-  // list.insert_beg(list.create_node(1));
-  cout << list.get_size() << endl;
+  double_list list;  
+  
+  // inserting test
+  list.insert_at(list.create_node(1), 1);
+  list.insert_at(list.create_node(2), 2);
+  list.insert_at(list.create_node(3), 2);
+  cout << "after inserting: " << endl;
+  cout << "Size: " << list.get_size() << endl;
+  list.print_list();
 
+  cout << "\n";
 
+  // deleting test
+  list.delete_at(1);
+  cout << "after delete pos 1: " << endl;
+  cout << "Size: " << list.get_size() << endl;
+  list.print_list();
+
+  cout << "\n";
+
+  // reversing test
+  list.reverse_list();
+  cout << "after reverse: " << endl;
+  cout << "Size: " << list.get_size() << endl;
+  list.print_list();
+
+  return 0;
 }
